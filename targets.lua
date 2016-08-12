2f2ecb2116f4f3a7be5a269b8b19e225e78066b9
Reed Weichler <rweichler@gmail.com>
2016-08-12 00:37:41 -0700

    change layout

--- targets.lua
+++ targets.lua
@@ -10,14 +10,16 @@ deb.packageinfo = {
     Maintainer = 'Reed Weichler <rweichler+cydia@gmail.com>',
     Author = 'Various Artists',
     Section = 'Development',
+    Depiction = 'http://github.com/rweichler/objc.lua',
 }
 deb.input = 'layout'
 deb.output = 'objc.lua.deb'
 
+local folder = '/usr/local/share/lua/5.1/objc'
 function default()
-    os.pexecute("rm -rf layout")
-    fs.mkdir("layout/var/lua/share/objc")
-    os.pexecute("cp src/*.lua layout/var/lua/share/objc/") 
+    os.pexecute("rm -rf "..deb.input)
+    fs.mkdir(deb.input..folder)
+    os.pexecute("cp src/*.lua "..deb.input..folder)
     deb:make_deb()
 end
 
